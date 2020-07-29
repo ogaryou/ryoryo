6fa5ce207c26292fa642d150b0055fac7f05d5c5
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-07-29 09:11:18 +0900

    header レイアウト作成

--- dawnSNS/resources/views/layouts/login.blade.php
+++ dawnSNS/resources/views/layouts/login.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="description" content="ページの内容を表す文章" />
     <title></title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{asset('css/reset.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <!--スマホ,タブレット対応-->
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <!--サイトのアイコン指定-->
@@ -23,24 +23,30 @@
     <header>
         
         <div id = "head">
-        <h1><a><img src="{{asset('images/main_logo.png')}}"></a></h1>
-            <div id="">
-                <div id="">
-                    <p>{{$username->username}}さん<img src="{{asset('images/dawn.png')}}"></p>
-                <div>
-     
-                <ul>
-                    <li><a href="/top">ホーム</a></li>
-                    <li><a href="/profile">プロフィール</a></li>
-                    <li><a href="/logout">ログアウト</a></li>
-                </ul>
+        <h1><a href="/top"><img src="{{asset('images/main_logo.png')}}" ></a></h1>
+            <div id="top-header">
+                <div id="top-link-header">
+                    <p class="name">{{$username->username}}さん</p>
+                    <img src="{{asset('images/dawn.png')}}" class="main-image">
+                </div>
+                <div class="drop-menu">
+                <ul class="top-menu-header">
+                    <li><a href="/top" class="top-link">ホーム</a></li>
+                    <li><a href="/profile" class="top-link">プロフィール</a></li>
+                    <li><a href="/logout" class="top-link">ログアウト</a></li>
+                </ul>  
             </div>
+            </div>
+
         </div>
     </header>
+
     <div id="row">
+
         <div id="container">
             @yield('content')
         </div >
+
         <div id="side-bar">
             <div id="confirm">
                 <p>{{$username->username}}さんの</p>
@@ -60,7 +66,8 @@
     </div>
     <footer>
     </footer>
-    <script src="JavaScriptファイルのURL"></script>
-    <script src="JavaScriptファイルのURL"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{asset('js/top.js')}}"></script>
 </body>
 </html>
+
