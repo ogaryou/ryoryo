9733dcb28a47f1310134bf99899f44f69a72747f
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-07-29 20:22:56 +0900

    リンクの設定

--- dawnSNS/app/Http/Controllers/UsersController.php
+++ dawnSNS/app/Http/Controllers/UsersController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class UsersController extends Controller
 {
     //
     
     public function profile(){
-        return view('users.profile');
+        $username = Auth::user();
+        return view('users.profile',['username'=> $username]);
     }
     public function search(){
         return view('users.search');
