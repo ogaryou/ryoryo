87eaaa14d2619a1e565a796945d7cf7ef343683e
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-09-23 10:26:31 +0900

    新規会員登録css

--- dawnSNS/resources/views/auth/register.blade.php
+++ dawnSNS/resources/views/auth/register.blade.php
@@ -4,55 +4,61 @@
 
 {!! Form::open() !!}
 <div class="form-layout">
-  <div class="form-h2">
-    <h2>新規ユーザー登録</h2>
-  </div>
-  <div class="form-error">
-    @if($errors->any())
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{$error}}</li>
-      @endforeach  
-    </ul>
-    @endif
-  </div>
+<div class="form-error">
+        @if($errors->any())
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+          @endforeach  
+        </ul>
+        @endif
+      </div>
 
   <div class="form-box">
-    <div class="form-name">
-      {{ Form::label('ユーザー名') }}
-    </div>
-    <div class="form-contents">
-      {{ Form::text('username',null,['class' => 'input']) }}
-    </div>
+    <div class="form-inline">
+        <div class="form-h2">
+        <h2>新規ユーザー登録</h2>
+      </div>
 
-    <div class="form-name">
-      {{ Form::label('メールアドレス') }}
-    </div>
-    <div class="form-contents">
-      {{ Form::text('mail',null,['class' => 'input']) }}
-    </div>
-    <div class="form-name">
-      {{ Form::label('パスワード') }}
-    </div>
-    <div class="form-contents">
-      {{ Form::text('password',null,['class' => 'input']) }}
-    </div>
+      <div class="form-name">
+        {{ Form::label('ユーザー名',null,['class' =>'name']) }}
+      </div>
+      <div class="form-contents">
+        {{ Form::text('username',null,['class' => 'input']) }}
+      </div>
 
-
-    <div class="form-name">
-      {{ Form::label('パスワード確認') }}
-    </div>
-    <div class="form-contents">
-      {{ Form::text('password_confirmation',null,['class' => 'input']) }}
-    </div>
+      <div class="form-name">
+        {{ Form::label('メールアドレス',null,['class' =>'name']) }}
+      </div>
+      <div class="form-contents">
+        {{ Form::text('mail',null,['class' => 'input']) }}
+      </div>
+      <div class="form-name">
+        {{ Form::label('パスワード',null,['class' =>'name']) }}
+      </div>
+      <div class="form-contents">
+        {{ Form::text('password',null,['class' => 'input']) }}
+      </div>
 
 
+      <div class="form-name">
+        {{ Form::label('パスワード確認',null,['class' =>'name']) }}
+      </div>
+      <div class="form-contents">
+        {{ Form::text('password_confirmation',null,['class' => 'input']) }}
+    
 
-    {{ Form::submit('登録') }}
+
+      <div class="form-contents">
+      {{ Form::submit('登録',['class'=> 'register-submit']) }}
+      </div>
+
+      <p><a href="/login" class="register-link">ログイン画面へ戻る</a></p>
+    </div>  
   </div>
 
 
-  <p><a href="/login">ログイン画面へ戻る</a></p>
+  
 
   {!! Form::close() !!}
 </div>
